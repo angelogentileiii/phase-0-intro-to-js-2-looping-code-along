2fe00324d23f0b0e310b28b3295023ddec34d438
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-08-16 18:37:43 -0400

    completed looping lab.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const thankYou = [];
+    for (let x = 0; x < names.length; x++){
+        thankYou.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;  
+};
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(number){
+    while (number > -1) {
+        console.log(number--);
+    }
+}
+
+countDown(10);
